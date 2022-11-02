6dc2717980bf021834a11c583dbdca191e6a4b29
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-11-02 19:12:42 +0100

    problema con execve

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 17:44:12 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/11/02 16:48:37 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/11/02 19:11:00 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -15,18 +15,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-void	child(int fdin, int fdout, int *pipe, char **ep, char **av)
+void	child(int fdin, int *pipe, char **ep, char **av)
 {
 	dup2(fdin, STDIN_FILENO);
-	dup2(fdout, STDOUT_FILENO);
+	dup2(pipe[1], STDOUT_FILENO);
 	close(pipe[0]);
-	close(pipe[1]);
-	write(STDOUT_FILENO, "Soy hijo 1\n", 11);
-	execve("/bin/cat", &av[1], ep);
-	printf("Error: execve failed\n");
+	execve("/bin/cat", &av[0], ep);
+	printf("Aqui si Error: execve failed\n");
 	exit(-1);
 }
 
+void	child2(int fdout, int *pipe, char **ep, char **av)
+{
+	dup2(pipe[0], STDIN_FILENO);
+	dup2(fdout, STDOUT_FILENO);
+	close(pipe[1]);
+	execve("/usr/bin/wc", &av[2], ep);
+	printf("Aqui Error: execve failed\n");
+	exit(-1);
+}
 int main(int ac, char **av, char **ep)
 {
 	int	fdin = open("infile", O_RDONLY);
@@ -34,7 +41,9 @@ int main(int ac, char **av, char **ep)
 	int	pp1[2];
 	int	pid;
 	int	status;
+	char *arg[4] = {"cat", "\0", "wc", "\0"};
 
+	(void)av;
 	(void)ac;
 	if (pipe(pp1) == 1)
 	{
@@ -44,7 +53,13 @@ int main(int ac, char **av, char **ep)
 	pid = fork();
 	if (pid == 0)
 	{
-		child(fdin, fdout, pp1, ep, av);
+		pid = fork();
+		if (pid == 0)
+		{
+			child(fdin, pp1, ep, arg);
+		}
+		waitpid(pid, &status, 0);
+		child2(fdout, pp1, ep, arg);
 	} 
 	close(pp1[1]);
 	close(pp1[0]);
