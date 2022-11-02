42bb3a281c2ab91470584b20e445afbbd1932ae5
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-11-02 16:50:53 +0100

    function child

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 17:44:12 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/11/02 12:27:07 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/11/02 16:48:37 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -15,12 +15,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+void	child(int fdin, int fdout, int *pipe, char **ep, char **av)
+{
+	dup2(fdin, STDIN_FILENO);
+	dup2(fdout, STDOUT_FILENO);
+	close(pipe[0]);
+	close(pipe[1]);
+	write(STDOUT_FILENO, "Soy hijo 1\n", 11);
+	execve("/bin/cat", &av[1], ep);
+	printf("Error: execve failed\n");
+	exit(-1);
+}
+
 int main(int ac, char **av, char **ep)
 {
-	int		fdin = open("infile", O_RDONLY);
-	int		fdout = open("outfile", O_WRONLY);
-	int		pp1[2];
-	int		pid;
+	int	fdin = open("infile", O_RDONLY);
+	int	fdout = open("outfile", O_WRONLY);
+	int	pp1[2];
+	int	pid;
+	int	status;
 
 	(void)ac;
 	if (pipe(pp1) == 1)
@@ -31,19 +44,13 @@ int main(int ac, char **av, char **ep)
 	pid = fork();
 	if (pid == 0)
 	{
-		dup2(fdin, STDIN_FILENO);
-		dup2(fdout, STDOUT_FILENO);
-		close(pp1[0]);
-		close(pp1[1]);
-		write(STDOUT_FILENO, "Soy hijo 1\n", 11);
-		execve("/bin/cat", &av[1], ep);
-		printf("Error: execve failed\n");
-		exit(-1);
+		child(fdin, fdout, pp1, ep, av);
 	} 
 	close(pp1[1]);
 	close(pp1[0]);
 	close(fdin);
 	close(fdout);
+	waitpid(pid, &status, 0);
 }
 //pp[0]--->lectura en pipe
 //pp[1]--->escritura en pipe
