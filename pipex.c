8c627676d3d6780605798b1ae6d80831bc20fd1d
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-11-03 15:11:09 +0100

    working on parsing PATH

--- pipex.c
+++ pipex.c
@@ -6,22 +6,19 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 17:44:12 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/11/03 10:39:24 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/11/03 15:10:23 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
+#include "pipex.h"
+
 
 void	child(int fdin, int *pipe, char **ep, char **av)
 {
 	dup2(fdin, STDIN_FILENO);
 	dup2(pipe[1], STDOUT_FILENO);
-	close(pipe[0]);
 	close(pipe[1]);
-	execve("/bin/cat", &av[0], ep);
+	close(pipe[0]);
+	execve(av[0], &av[0], ep);
 	printf("Error 1: execve failed\n");
 	exit(-1);
 }
@@ -29,22 +26,27 @@ void	child(int fdin, int *pipe, char **ep, char **av)
 void	child2(int fdout, int *pipe, char **ep, char **av)
 {
 	dup2(fdout, STDOUT_FILENO);
-	printf("pipe: %d\n", pipe[0]);
 	dup2(pipe[0], STDIN_FILENO);
 	close(pipe[1]);
 	close(pipe[0]);
-	execve("/usr/bin/wc", &av[2], ep);
+	execve(av[0], &av[0], ep);
 	printf("Error 2: execve failed\n");
 	exit(-1);
 }
+
+// void	findpath(char **ep)
+// {
+//     
+// }
+
 int main(int ac, char **av, char **ep)
 {
-	int	fdin = open("infile", O_RDONLY);
-	int	fdout = open("outfile", O_WRONLY);
-	int	pp1[2];
-	int	pid;
-	int	status;
-	char *arg[4] = {"cat", "\0", "wc", "\0"};
+	int		fdin = open("infile", O_RDONLY);
+	int		fdout = open("outfile", O_WRONLY);
+	int		pp1[2];
+	int		pid;
+	int		status;
+	char	*argv[] = {"/bin/cat", "-e", 0, "/usr/bin/wc", "-l", 0};
 
 	(void)av;
 	(void)ac;
@@ -59,10 +61,10 @@ int main(int ac, char **av, char **ep)
 		pid = fork();
 		if (pid == 0)
 		{
-			child(fdin, pp1, ep, arg);
+			child(fdin, pp1, ep, &argv[0]);
 		}
 		waitpid(pid, &status, 0);
-		child2(fdout, pp1, ep, arg);
+		child2(fdout, pp1, ep, &argv[3]);
 	} 
 	close(pp1[1]);
 	close(pp1[0]);
---
+++ pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 15:03:43 by vde-prad          #+#    #+#             */
+/*   Updated: 2022/11/03 15:06:39 by vde-prad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+//*********************** STANDARD LIBRARIES *********************************
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+//*************************** FUNCTIONS **************************************
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
+#endif
